425453972e44fe4eb3304f717ea4ef68ce449ec2
Nishwanth2809 <Nishwanth2809@users.noreply.github.com>
2026-02-19 22:43:53 +0530

    Fix CSS import case for Vercel Linux build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./pages/upload.css";
+import "./pages/Upload.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
